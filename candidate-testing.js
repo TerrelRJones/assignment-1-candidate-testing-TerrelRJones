391abcf2410c149427cb41e843540283ff534b68
Terrel <TerrelRJones@gmail.com>
2021-06-03 01:39:09 +0000

    Assigned variables and finished Part 1: MVQ

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,12 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,11 +16,16 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+    let candidateName = input.question('What is your name? ');
 
+    return candidateName;
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  let candidateAnswer = input.question(question);
+
+  return candidateAnswer;
 
 
 }
@@ -27,7 +33,10 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+  if (askQuestion() === 'Sally Ride'){
+    console.log('Congratulations, you were right!') }else {
+      console.log('Sorry, you are wrong.');
+    };
 
   let grade;
   
@@ -36,10 +45,12 @@ function gradeQuiz(candidateAnswers) {
 }
 
 function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
   
-  askQuestion();
+  // TODO 1.1c: Ask for candidate's name //
+
+  console.log('Hello, ' + askForName() + '!' + ' Welcome!');
+
+  // askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
 
